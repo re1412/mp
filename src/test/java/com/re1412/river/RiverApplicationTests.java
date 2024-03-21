0c7a1e6573ddcbaa0ece622b8704002c89b4144c
re1412 <re1412@naver.com>
2024-03-21 16:10:54 +0900

    git commit

---
+++ src/test/java/com/re1412/river/RiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re1412.river;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
